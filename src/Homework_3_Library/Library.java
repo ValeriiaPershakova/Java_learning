6d30399438aa840e7627347a7a4bc5820567a5a3
ValeriiaPershakova <kagami1603@gmail.com>
2019-08-07 17:15:54 +0300

    Homework 3. Library. Undone

---
+++ src/Homework_3_Library/Library.java
@@ -0,0 +1,33 @@
+package Homework_3_Library;
+
+import java.util.ArrayList;
+
+
+public class Library {
+    public static ArrayList<Book> AllBooks = new ArrayList<Book>();
+    public static ArrayList<Author> AllAuthors = new ArrayList<Author>();
+
+
+
+
+
+
+
+    public void printAllBooks() {
+
+    }
+
+    public void printAllAuthors() {
+
+    }
+
+    public void printAllBooksAndAuthors() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
